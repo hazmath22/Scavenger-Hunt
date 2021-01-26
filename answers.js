7d80cf3fd17a411719f27dda2152cc17aeea36ae
hazmath22 <67228488+hazmath22@users.noreply.github.com>
2021-01-26 21:54:01 +0500

    Create answers.js

---
+++ answers.js
@@ -0,0 +1,147 @@
+function check_ans1(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Thor Ragnarok" || answer.value == "thor ragnarok" || answer.value == "Thor ragnarok"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans2(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "666"){
+    right.style.display = "block";
+    next.style.display = "inline";
+    wrong.style.display = "none";
+  }
+  else{
+    wrong.style.display = "block";
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans3(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "1132"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans4(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Starry night" || answer.value == "starry night"){
+    right.style.display = "block";
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans5(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "42069"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans6(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "134340"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    next.style.display = "none";
+  }
+}
+function check_ans7(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Luxembourg" || answer.value == "luxembourg"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    next.style.display = "none";
+  }
+}
+function check_ans8(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "MK" || answer.value == "mk"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    next.style.display = "none";
+  }
+}
+function check_ans9(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "lethal" || answer.value == "Lethal"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    next.style.display = "none";
+  }
+}
+function check_ans10(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "truth" || answer.value == "Truth"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    next.style.display = "none";
+  }
+}
