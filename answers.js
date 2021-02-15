4e50be46b301c1d6aee1c61dab1049bb30795cb2
hazmath22 <67228488+hazmath22@users.noreply.github.com>
2021-02-15 15:22:58 +0500

    Update answers.js

--- answers.js
+++ answers.js
@@ -143,26 +143,4 @@ function check_ans10(){
     wrong.style.display = "none"
   }
  
-  <body oncontextmenu="return false">
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
-  else{
-    wrong.style.display = "block"
-    next.style.display = "none";
-  }
-}
+  
